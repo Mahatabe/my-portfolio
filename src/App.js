26e9e4dff89a10fc57b29507792df9f3b3f6dc0c
Mostafa Mahatabe <mahatabe.omi@gmail.com>
2023-03-26 17:31:14 +0600

    Header Complete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
-import Navbar from './components/Navbar';
+import Particles from "react-particles";
+import Navbar from "./components/Navbar";
+import Header from "./components/Header";
 
 function App() {
   return (
-    <Navbar />
+    <>
+      <Particles 
+      className="particles-canvas"
+        params={{
+          particles: {
+            number: {
+              value: 200,
+              density: {
+                enable: true,
+                value_area: 900,
+              }
+            },
+            shape:{
+              type: "Circle",
+              stroke: {
+                width: 6,
+                color: "#f9ab00",
+              }
+            }
+          },
+        }}
+      />
+      <Navbar />
+      <Header />
+    </>
   );
 }
 
