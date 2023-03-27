808f0660b0791e5d6b745bcba4eedba405489434
Mostafa Mahatabe <mahatabe.omi@gmail.com>
2023-03-27 15:01:35 +0600

    Abou me completed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,33 +6,33 @@ import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
     return (
-        <nav class="navbar navbar-expand-lg navbar-light bg-dark">
+        <nav className="navbar navbar-expand-lg navbar-light bg-dark">
             <div className="container">
-                <a class="navbar-brand" href="#"><img class="logo" src={logo} alt="logo.." /></a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse"
+                <a className="navbar-brand" href="#"><img className="logo" src={logo} alt="logo.." /></a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse"
                     data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
                     aria-expanded="false" aria-label="Toggle navigation">
                     <FontAwesomeIcon icon={faBars} style={{color: "#fff"}} />
                 </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav ms-auto">
-                        <li class="nav-item active">
-                            <a class="nav-link" href="#">Home</a>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav ms-auto">
+                        <li className="nav-item active">
+                            <a className="nav-link" href="#">Home</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">About Me</a>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">About Me</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">My Work</a>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">My Work</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Experience</a>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Experience</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Portfolio</a>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Portfolio</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Contact</a>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Contact</a>
                         </li>
                     </ul>
                 </div>
