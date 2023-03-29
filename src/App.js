33079966869d22db48c86098c0af17d09477c0b3
Mostafa Mahatabe <mahatabe.omi@gmail.com>
2023-03-29 16:50:14 +0600

    Footer completed

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Navbar from "./components/Navbar";
 import Header from "./components/Header";
 import AboutMe from "./components/AboutMe";
 import Services from "./components/Services";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -34,6 +35,7 @@ function App() {
       <Header />
       <AboutMe />
       <Services />
+      <Footer />
     </>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -23,7 +23,7 @@ const Navbar = () => {
                             <a className="nav-link" href="#">About Me</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#">My Work</a>
+                            <a className="nav-link" href="#">Services</a>
                         </li>
                         <li className="nav-item">
                             <a className="nav-link" href="#">Experience</a>
